2b1b62f6331bc235f9c4b1cd69e9590d272fdbee
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-09-06 21:18:32 -0500

    hash tables

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-const MyArray = require("./DataStructures/MyArray.js");
+// Arrays
+const MyArray = require("./src/array/array");
+// Hastable
+const Hashtable = require("./src/hashtable/hashtable");
 
-const myArray = new MyArray();
-
-myArray.push("Edilberto");
-myArray.push("Maria");
-myArray.push("Ana");
-console.log(myArray);
+// Arrays
+// const myArray = new MyArray();
+// myArray.push("Edilberto");
+// myArray.push("Maria");
+// myArray.push("Ana");
+// console.log(myArray);
 // myArray.pop();
 // myArray.delete(1);
 // myArray.shift();
-myArray.splice(3, "Juan");
-console.log(myArray);
+// myArray.splice(3, "Juan");
+// console.log(myArray);
+
+// Hashtable
+const myHashtable = new Hashtable(50);
+myHashtable.set("Edilberto", 1998);
+myHashtable.set("Diego", 1990);
+myHashtable.set("Mariana", 1998);
+console.log(myHashtable.getHashes());
+myHashtable.delete("Mariana");
+console.log(myHashtable.getHashes());
+// console.log(myHashtable.get("Edilberto"));
